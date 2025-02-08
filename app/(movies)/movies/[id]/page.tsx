a5a5bc11622653a053dadc9e636e64a515117aca
Jneck <parklim2254@gmail.com>
2025-02-08 21:43:13 +0900

    param 로딩 방식 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,9 +2,16 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
+export interface IParams {
+    params: 
+        Promise<{id: string}>
+    
+}
+
 export async function generateMetadata({
-    params: {id},
-    }: {params: {id: string}}){
+    params,
+    }: IParams){
+        const id = (await params).id
         const movie = await getMovie(id)
     return {
         title: movie.title,
@@ -12,8 +19,9 @@ export async function generateMetadata({
 }
 
 export default async function MovieDetail({
-params: {id},
-}: {params: {id: string}}) {
+params,
+}: IParams) {
+    const id = (await params).id
     return <div>
         <Suspense fallback={<h1>Loading Movie info </h1>}>
         <MovieInfo id={id}/>    
